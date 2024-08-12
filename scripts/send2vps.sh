bb7d1ee186e6be69c7ef8f6b3e3ba7a6ab3920f7
Matt Slater <github@mslater.fastmail.com>
2024-08-12 12:55:20 +0100

    Make send2vps script check for spaces in filename

--- scripts/send2vps.sh
+++ scripts/send2vps.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-FILE_TO_SEND=$1
+FILE_TO_SEND="$1"
 
 if [ -z "$FILE_TO_SEND" ]; then
     kdialog --title "Send what?" --error "There was no file specified to send."
 else
-    scp -P YOUR_SSH_PORT $FILE_TO_SEND YOUR_HOST_HERE:/path/to/your/public/dir
+    scp -P YOUR_SSH_PORT "$FILE_TO_SEND" YOUR_HOST_HERE:/path/to/your/public/dir
     if [ $? -eq 0 ]; then
         FILENAME="${FILE_TO_SEND##*/}"
-        echo "https://YOUR_URL_HERE/$FILENAME" | xclip -r -selection c
+        if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+            echo "https://plzlookat.mystupid.pictures/$FILENAME" | wl-copy
+        else
+            echo "https://plzlookat.mystupid.pictures/$FILENAME" | xclip -r -selection c
+        fi
         kdialog --title "Link copied" --passivepopup "Link to the file has been copied to the clipboard." 5
     else
         kdialog --title "Link copied" --error "There was an error transferring the file. Did you set up your parameters correctly?"
